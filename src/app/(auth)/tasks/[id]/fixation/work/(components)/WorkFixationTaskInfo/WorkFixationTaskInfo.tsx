8fbcb439e08593dac9ac3a2b87baddcd483ff854
vitaliy ivanof <ivitalij741@gmail.com>
2024-06-26 00:15:39 +0700

    Add move to prev task button

--- src/app/(auth)/tasks/[id]/fixation/work/(components)/WorkFixationTaskInfo/WorkFixationTaskInfo.tsx
+++ src/app/(auth)/tasks/[id]/fixation/work/(components)/WorkFixationTaskInfo/WorkFixationTaskInfo.tsx
@@ -1,38 +1,48 @@
 'use client';
 
-import { Typography } from '@/components/ui';
+import { Button, Typography } from '@/components/ui';
 import { cn } from '@/lib/utils';
 import { dateFormat } from '@/shared/helpers/dateFormat';
 import { getTaskStatusColor, getTaskStatusText } from '@/shared/helpers/getTaskStatusColor';
 import { FixationWorkTaskDTO } from '@generated/api';
+import { getNextFlowLinkByTask } from '@/features/inspectorTasks/helpers/getNextFlowLinkByTask';
+import Link from 'next/link';
 
 interface IWorkFixationTaskInfo {
   data: FixationWorkTaskDTO;
 }
 
-const WorkFixationTaskInfo = ({ data }: IWorkFixationTaskInfo) => (
-  <div>
-    <Typography tag='h3' variant='h3' className='mb-2 text-center'>
-      Подробности задачи
-    </Typography>
-    <div className='p-1'>
-      <div className='flex justify-between'>
-        <Typography tag='h6' variant='h7'>
-          Фиксация выполненных работ
+const WorkFixationTaskInfo = ({ data }: IWorkFixationTaskInfo) => {
+  return (
+    <>
+      <Typography tag='h3' variant='h3' className='mb-2 text-center'>
+        Подробности задачи
+      </Typography>
+      <div className='p-1'>
+        <div className='flex justify-between'>
+          <Typography tag='h6' variant='h7'>
+            Фиксация выполненных работ
+          </Typography>
+          <Typography tag='h5' variant='h7' className={cn(getTaskStatusColor(data.taskStatus))}>
+            {getTaskStatusText(data.taskStatus!)}
+          </Typography>
+        </div>
+        <Typography tag='p' variant='sub3' className='my-1'>
+          {data.address}
         </Typography>
-        <Typography tag='h5' variant='h7' className={cn(getTaskStatusColor(data.taskStatus))}>
-          {getTaskStatusText(data.taskStatus!)}
-        </Typography>
+        <Typography tag='p' variant='sub3'>
+          Создано {dateFormat(new Date(data.createdDateTime))}
+        </Typography>        
       </div>
-      <Typography tag='p' variant='sub3' className='my-1'>
-        {data.address}
-      </Typography>
-      <Typography tag='p' variant='sub3'>
-        Создано {dateFormat(new Date(data.createdDateTime))}
-      </Typography>
-    </div>
-  </div>
-);
+
+      <Link href={getNextFlowLinkByTask(data.prevTask)} className='flex flex-col'>
+        <Button size='sm' className='rounded-full px-[10px] py-2 mt-3 w-100'>
+          Перейти на предыдущую задачу
+        </Button>
+      </Link>
+    </>
+  );
+}
 
 export default WorkFixationTaskInfo;
 
