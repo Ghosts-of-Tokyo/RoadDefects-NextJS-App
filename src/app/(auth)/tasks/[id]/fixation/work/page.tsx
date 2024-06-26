15d43d1bd9406bf43ffa54e9f46b452e3844a595
vitaliy ivanof <ivitalij741@gmail.com>
2024-06-26 21:13:22 +0700

    feature/adding-contractor-to-fixation 🤠 Add contractor to fixation work

--- src/app/(auth)/tasks/[id]/fixation/work/page.tsx
+++ src/app/(auth)/tasks/[id]/fixation/work/page.tsx
@@ -12,6 +12,7 @@ import { DefectFixationEditForm } from './(components)/DefectFixationEditForm/De
 import { FixationWorkEditForm } from './(components)/DefectWorkEditForm/FixationWorkEditForm';
 import WorkFixationTaskInfo from './(components)/WorkFixationTaskInfo/WorkFixationTaskInfo';
 import { useFixationWorkEditPage } from './(hooks)/useFixationWorkEditPage';
+import ContractorCard from '@/app/(auth)/fixation/defect/[defectId]/contractors/(components)/ContractorCard/ContractorCard';
 
 const DefectWorkFixationPage = () => {
   const { state, functions } = useFixationWorkEditPage();
@@ -121,6 +122,27 @@ const DefectWorkFixationPage = () => {
         onUpdateTaskStatusClick={functions.onUpdateTaskStatusClick}
         updateTaskStatus={state.isLoading.updateTaskStatus}
       />
+
+      {state.data.data.taskStatus === 'Completed' && state.data.data.defectFixation && !state.data.data.defectFixation.contractor && (
+        <Link href={ROUTES.FIXATION_DEFECT.CONTRACTORS(state.data.data.defectFixation.id)}>
+          <Button type='submit' size='lg' className='w-full'>
+            Выбрать подрячика для выполнения работ
+          </Button>
+        </Link>
+      )}
+
+      {state.data.data.taskStatus === 'Completed' && state.data.data.defectFixation && state.data.data.defectFixation.contractor && (
+        <>
+          <Typography
+            tag='p'
+            variant='sub2'
+            className='text-center text-gray-500'
+          >
+            Подрядчик, назначенный на устранение дефекта
+          </Typography>
+          <ContractorCard contractor={state.data.data.defectFixation.contractor} />
+        </>        
+      )}
     </div>
   );
 };
