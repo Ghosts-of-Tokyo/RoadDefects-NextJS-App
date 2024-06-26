c1da68902432ae91302afd89a31990e1e76686ac
vitaliy ivanof <ivitalij741@gmail.com>
2024-06-26 22:13:21 +0700

    feature/adding-prev-task-button 🤠 Add move to prev task button

--- src/app/(auth)/tasks/[id]/fixation/work/(components)/WorkFixationTaskInfo/WorkFixationTaskInfo.tsx
+++ src/app/(auth)/tasks/[id]/fixation/work/(components)/WorkFixationTaskInfo/WorkFixationTaskInfo.tsx
@@ -2,9 +2,11 @@
 
 import type { FixationWorkTaskDTO } from '@generated/api';
 
-import { Typography } from '@/components/ui';
+import { Button, Typography } from '@/components/ui';
 import { cn } from '@/lib/utils';
 import { getTaskStatusColor, getTaskStatusText } from '@/shared/helpers/getTaskStatusColor';
+import Link from 'next/link';
+import { getNextFlowLinkByTask } from '@/features/inspectorTasks/helpers/getNextFlowLinkByTask';
 
 interface IWorkFixationTaskInfo {
   data: FixationWorkTaskDTO;
@@ -31,6 +33,12 @@ const WorkFixationTaskInfo = ({ data }: IWorkFixationTaskInfo) => (
         {new Date(data.createdDateTime).toLocaleString()}
       </Typography>
     </div>
+
+    <Link href={getNextFlowLinkByTask(data.prevTask)} className='flex flex-col'>
+        <Button size='sm' className='rounded-full px-[10px] py-2 mt-3 w-100'>
+          Перейти на предыдущую задачу
+        </Button>
+      </Link>
   </div>
 );
 
